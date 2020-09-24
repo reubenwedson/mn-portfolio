32c0a03b9d2da3194bc6e2012adbb63d3937d133
reubenwedson <reubenwedson@gmail.com>
2020-09-24 14:48:45 +0300

    Fixing syntax errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
+// import Contact from './Components/Contact';
+// import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
