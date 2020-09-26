d69f82d9c99895a21a63dffe0cef0e0a0f7595ce
reubenwedson <reubenwedson@gmail.com>
2020-09-26 23:29:53 +0300

    Update portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/'+hostedAndEditedByMe.image;
         return <div key={hostedAndEditedByMe.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={hostedAndEditedByMe.url} title={hostedAndEditedByMe.title}>
+            <a href={hostedAndEditedByMe.url} title={hostedAndEditedByMe.title} target="_blank"  rel="noopener noreferrer">
                <img alt={hostedAndEditedByMe.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
@@ -16,7 +16,6 @@ class Portfolio extends Component {
                      <p>{hostedAndEditedByMe.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
         </div>
@@ -25,7 +24,7 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/'+shotAndEditedByMe.image;
         return <div key={shotAndEditedByMe.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={shotAndEditedByMe.url} title={shotAndEditedByMe.title}>
+            <a href={shotAndEditedByMe.url} title={shotAndEditedByMe.title} target="_blank"  rel="noopener noreferrer">
                <img alt={shotAndEditedByMe.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
@@ -33,7 +32,38 @@ class Portfolio extends Component {
                      <p>{shotAndEditedByMe.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+      var shotByMe = this.props.data.shotByMe.map(function(shotByMe){
+        var projectImage = 'images/portfolio/'+shotByMe.image;
+        return <div key={shotByMe.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={shotByMe.url} title={shotByMe.title} target="_blank"  rel="noopener noreferrer">
+               <img alt={shotByMe.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{shotByMe.title}</h5>
+                     <p>{shotByMe.category}</p>
+                  </div>
+                </div>
+            </a>
+          </div>
+        </div>
+      })
+        var producedByMe = this.props.data.producedByMe.map(function(producedByMe){
+        var projectImage = 'images/portfolio/'+producedByMe.image;
+        return <div key={producedByMe.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={producedByMe.url} title={producedByMe.title} target="_blank"  rel="noopener noreferrer">
+               <img alt={producedByMe.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{producedByMe.title}</h5>
+                     <p>{producedByMe.category}</p>
+                  </div>
+                </div>
             </a>
           </div>
         </div>
@@ -63,6 +93,30 @@ class Portfolio extends Component {
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {shotAndEditedByMe}
+            </div>
+          </div>
+      </div>
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Shot by me.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {shotByMe}
+            </div>
+          </div>
+      </div>
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Produced by me.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {producedByMe}
             </div>
           </div>
       </div>
