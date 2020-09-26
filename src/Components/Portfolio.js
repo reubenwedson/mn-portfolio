223902807bec0878ce10f2ee5b2ee2390c1cf74d
reubenwedson <reubenwedson@gmail.com>
2020-09-26 18:32:33 +0300

    Update portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,16 +4,33 @@ class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
+      var hostedAndEditedByMe = this.props.data.hostedAndEditedByMe.map(function(hostedAndEditedByMe){
+        var projectImage = 'images/portfolio/'+hostedAndEditedByMe.image;
+        return <div key={hostedAndEditedByMe.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
+            <a href={hostedAndEditedByMe.url} title={hostedAndEditedByMe.title}>
+               <img alt={hostedAndEditedByMe.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                 <h5>{hostedAndEditedByMe.title}</h5>
+                     <p>{hostedAndEditedByMe.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+      var shotAndEditedByMe = this.props.data.shotAndEditedByMe.map(function(shotAndEditedByMe){
+        var projectImage = 'images/portfolio/'+shotAndEditedByMe.image;
+        return <div key={shotAndEditedByMe.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={shotAndEditedByMe.url} title={shotAndEditedByMe.title}>
+               <img alt={shotAndEditedByMe.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{shotAndEditedByMe.title}</h5>
+                     <p>{shotAndEditedByMe.category}</p>
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
@@ -30,10 +47,22 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Hosted and edited by me.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+                {hostedAndEditedByMe}
+            </div>
+          </div>
+      </div>
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Shot and edited by me.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {shotAndEditedByMe}
             </div>
           </div>
       </div>
